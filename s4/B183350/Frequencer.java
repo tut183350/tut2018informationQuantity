6f2d5c397115f07afe4641bf2255425e177fc761
tut183350 <prelude.toshi.mc@gmail.com>
2018-12-18 21:17:27 +0900

    TestCase.java: for Frequencer

--- s4/B183350/Frequencer.java
+++ s4/B183350/Frequencer.java
@@ -2,18 +2,6 @@ package s4.B183350; // Please modify to s4.Bnnnnnn, where nnnnnn is your student
 import java.lang.*;
 import s4.specification.*;
 
-/*
-interface FrequencerInterface {     // This interface provides the design for frequency counter.
-    void setTarget(byte[]  target); // set the data to search.
-    void setSpace(byte[]  space);  // set the data to be searched target from.
-    int frequency(); //It return -1, when TARGET is not set or TARGET's length is zero
-                    //Otherwise, it return 0, when SPACE is not set or Space's length is zero
-                    //Otherwise, get the frequency of TAGET in SPACE
-    int subByteFrequency(int start, int end);
-    // get the frequency of subByte of taget, i.e target[start], taget[start+1], ... , target[end-1].
-    // For the incorrect value of START or END, the behavior is undefined.
-*/
-
 
 public class Frequencer implements FrequencerInterface{
     // Code to Test, *warning: This code  contains intentional problem*
@@ -22,6 +10,7 @@ public class Frequencer implements FrequencerInterface{
     public void setTarget(byte [] target) { myTarget = target;}
     public void setSpace(byte []space) { mySpace = space; }
     public int frequency() {
+	//targetLength > spaceLengthでException
 	int targetLength = myTarget.length;
 	int spaceLength = mySpace.length;
 	int count = 0;
@@ -59,3 +48,14 @@ public class Frequencer implements FrequencerInterface{
     }
 }	    
 	    
+/*
+interface FrequencerInterface {     // This interface provides the design for frequency counter.
+    void setTarget(byte[]  target); // set the data to search.
+    void setSpace(byte[]  space);  // set the data to be searched target from.
+    int frequency(); //It return -1, when TARGET is not set or TARGET's length is zero
+                    //Otherwise, it return 0, when SPACE is not set or Space's length is zero
+                    //Otherwise, get the frequency of TAGET in SPACE
+    int subByteFrequency(int start, int end);
+    // get the frequency of subByte of taget, i.e target[start], taget[start+1], ... , target[end-1].
+    // For the incorrect value of START or END, the behavior is undefined.
+*/
--- s4/B183350/TestCase.java
+++ s4/B183350/TestCase.java
@@ -2,34 +2,6 @@ package s4.B183350; // Please modify to s4.Bnnnnnn, where nnnnnn is your student
 import java.lang.*;
 import s4.specification.*;
 
-/*
-interface FrequencerInterface {     // This interface provides the design for frequency counter.
-    void setTarget(byte[]  target); // set the data to search.
-    void setSpace(byte[]  space);  // set the data to be searched target from.
-    int frequency(); //It return -1, when TARGET is not set or TARGET's length is zero
-                    //Otherwise, it return 0, when SPACE is not set or Space's length is zero
-                    //Otherwise, get the frequency of TAGET in SPACE
-    int subByteFrequency(int start, int end);
-    // get the frequency of subByte of taget, i.e target[start], taget[start+1], ... , target[end-1].
-    // For the incorrect value of START or END, the behavior is undefined.
-}
-*/
-
-
-/*
-package s4.specification;
-public interface InformationEstimatorInterface{
-    void setTarget(byte target[]); // set the data for computing the information quantities
-    void setSpace(byte space[]); // set data for sample space to computer probability
-    double estimation(); // It returns 0.0 when the target is not set or Target's length is zero;
-// It returns Double.MAX_VALUE, when the true value is infinite, or space is not set.
-// The behavior is undefined, if the true value is finete but larger than Double.MAX_VALUE.
-// Note that this happens only when the space is unreasonably large. We will encounter other problem anyway.
-// Otherwise, estimation of information quantity, 
-}
-*/
-
-
 public class TestCase {
     public static void main(String[] args) {
 	try {
